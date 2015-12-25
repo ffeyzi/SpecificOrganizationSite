968901712e06df0ca85923e574dbf75a4dcaa413
ffeyzi <fffeyzi@gmail.com>
2015-12-24 18:14:30 -0800

    Gallery page and functionality done

--- js/slider.js
+++ js/slider.js
@@ -4,6 +4,7 @@
 
 var width = 900;
 var pauseTime = 4000;
+var currentSlide = 1;
 
 //cache DOM
 var $slider = $('#slider');
@@ -12,8 +13,26 @@ var $slidesContainer = $slider.find('.slides');
 //each individual slide
 var $slides = $slidesContainer.find('.slide');
 
-$(function() {
-	setInterval(function() {
-		$slidesContainer.animate({'margin-left': '-='+width}, 1000);
+//set up interval in global scope
+var interval;
+
+function startSlider() {
+	interval = setInterval(function() {
+		$slidesContainer.animate({'margin-left': '-='+width}, 1000, function() {
+			currentSlide++;
+			if (currentSlide == $slides.length) {
+				currentSlide = 1;
+				$slidesContainer.css('margin-left', 0);
+			}
+		});
 	}, pauseTime);
-});
+}
+
+function stopSlider() {
+	clearInterval(interval);
+}
+
+$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+//in case user refreshes the page
+startSlider();
